c9fe5e55d0b70a785bbcbe7e23e9bbc56abb1224
okdol1 <okdol0611@gmail.com>
2022-06-08 11:38:26 +0900

    gh-pages 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup';
 
+function Header(){
+  return <header><h1><a href="/">WWW</a></h1></header>
+}
+// function Nav(props){
+//   const list = props.data.map((e)=>{
+//     return <li key={e.id}><a href={'/read/'+e.id}>{e.title}</a></li>
+//   });
+//   return <nav>
+//     <ol>
+//         {list}
+//     </ol>
+//   </nav>
+// }
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+function Nav(props){
+  const liTags = props.data.map((e)=>{
+    return <li><a href={'/read/'+e.id}>{e.title}</a></li>
+  })
+  return <nav><ol>{liTags}</ol></nav>
+}
 function App() {
+  // const topics = [
+  //   {id:1, title:'html', body:'html is ...'},
+  //   {id:2, title:'css', body:'css is ...'}
+  // ];
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      {/* <Nav data={topics}></Nav> */}
+      <Nav data={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB!"></Article>
+      <ButtonGroup>
+        <Button variant="outlined">Create</Button>
+        <Button variant="outlined">Update</Button>
+      </ButtonGroup>
+      <Button variant="outlined">Delete</Button>
     </div>
   );
 }
